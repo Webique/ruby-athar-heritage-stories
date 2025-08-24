de52bad09f373f3f5fd834657765da97e7b8bd4b
Yosuf AlAwadi <yosuf.alawadi@student.manchester.ac.uk>
2025-08-24 03:36:40 +0300

    fix

--- railway-backend/index.js
+++ railway-backend/index.js
@@ -27,6 +27,7 @@ const corsOptions = {
       'http://localhost:3000',
       'http://localhost:5173',
       'https://rubyathar.netlify.app',
+      'https://atharruby.com',
       process.env.CORS_ORIGIN
     ].filter(Boolean);
     
